63813d072e366f4eab8075196efbc17bc317bd77
Maria Jose Melgarejo <mariajose.melgarejo@konecta.com.py>
2021-06-12 14:52:45 -0400

    Agregando categoriaProductos y producto

--- back-end-nodejs/routes/index.js
+++ back-end-nodejs/routes/index.js
@@ -2,11 +2,15 @@ const restaurantes = require('./restaurantes.routes');
 const mesas = require('./mesas.routes');
 const clientes = require('./clientes.routes');
 const reservas = require('./reservas.routes');
+const categoriaProductos = require('./categoria-productos.routes');
+const productos = require('./productos.routes');
 
 module.exports = app => {
     app.use('/api/mesas', mesas);
     app.use('/api/clientes', clientes);
     app.use('/api/reservas', reservas);
     app.use('/api/restaurantes', restaurantes);
+    app.use('/api/categoriaProductos', categoriaProductos);
+    app.use('/api/productos', productos);
     return app;
 }
